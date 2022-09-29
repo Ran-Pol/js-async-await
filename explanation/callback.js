c7e891dd1e247234b5a4c45a7d257cedc634c4ff
Ran Pol <rnd809@gmail.com>
2022-09-29 10:39:27 -0400

    Describe callback hel

---
+++ explanation/callback.js
@@ -0,0 +1,22 @@
+function requestHandler(req, res) {
+  user.findById(req.userId, function (err, user) {
+    if (err) {
+      res.send(err)
+    } else {
+      TextTrackList.findById(user.taskId, function (err, tasks) {
+        if (err) {
+          return res.send(err)
+        } else {
+          tasks.completed = true
+          tasks.save(function (err) {
+            if (err) {
+              return res.send(err)
+            } else {
+              res.send('Task completed')
+            }
+          })
+        }
+      })
+    }
+  })
+}
